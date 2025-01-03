e759c940a0a46de98a4066f0f64bb3a90bfae822
SilverLPs <36015913+SilverLPs@users.noreply.github.com>
2025-01-04 00:15:01 +0100

    Download module finished but needs testing of the 4 possible scenarios authenticated yes no and file or folder

--- download.sh
+++ download.sh
@@ -1,14 +1,24 @@
 VAR_SOURCE=""
 VAR_DESTINATION=""
+VAR_USERNAME=""
+VAR_PASSWORD=""
 
 for arg in "$@"; do
   case $arg in
     SOURCE=*)
-      VAR_SOURCE="$(realpath ${arg#*=})"
+      VAR_SOURCE="${arg#*=}"
       shift
       ;;
     DESTINATION=*)
       VAR_DESTINATION="$(realpath ${arg#*=})"
+      shift
+      ;;
+    USERNAME=*)
+      VAR_USERNAME="${arg#*=}"
+      shift
+      ;;
+    PASSWORD=*)
+      VAR_PASSWORD="${arg#*=}"
       shift
       ;;
   esac
@@ -16,16 +26,70 @@ done
 
 echo "MODULE INFO:"
 echo
-echo "Module:      bisonbackup.general.copy"
+echo "Module:      bisonbackup.networktransfer.download"
 echo "Path:        $(pwd)"
 echo "SOURCE:      $VAR_SOURCE"
 echo "DESTINATION: $VAR_DESTINATION"
-echo
-cp -rv "$VAR_SOURCE" "$VAR_DESTINATION"
-if [ $? -eq 0 ]; then
-    echo "Seems like the copy $VAR_DESTINATION was successfully created (Exit code 0)"
+echo "USERNAME:    $VAR_USERNAME"
+if [[ -n "$VAR_PASSWORD" ]]; then
+  echo "PASSWORD:    MD5=$(echo -n $VAR_PASSWORD | md5sum | awk '{print $1}')"
+else
+  echo "PASSWORD:    "
 fi
+echo
 
+lftp_authentication() {
+if [[ "${VAR_SOURCE: -1}" == "/" ]]; then
+  lftp <<EOF
+set ftp:list-options -a
+set sftp:auto-confirm yes
+set cmd:fail-exit yes
+open "$VAR_SOURCE" --user "$VAR_USERNAME" --password "$VAR_PASSWORD"
+mirror -P 2 --use-pget-n=1 --no-umask --no-perms "." "$VAR_DESTINATION"
+exit
+EOF
+else
+  lftp <<EOF
+set ftp:list-options -a
+set sftp:auto-confirm yes
+set cmd:fail-exit yes
+open "$(dirname $VAR_SOURCE)" --user "$VAR_USERNAME" --password "$VAR_PASSWORD"
+get "$(basename $VAR_SOURCE)" -o "$VAR_DESTINATION"
+exit
+EOF
+fi
+if [ $? -eq 0 ]; then
+    echo "Seems like the filetransfer finished successfully (Exit code 0)"
+fi
+}
 
-#download (ftp/http, files and directories)
-#-NEXT MODULES: Git (clone with all commits), BorgBackup
+lftp_no_authentication() {
+if [[ "${VAR_SOURCE: -1}" == "/" ]]; then
+  lftp <<EOF
+set ftp:list-options -a
+set sftp:auto-confirm yes
+set cmd:fail-exit yes
+mirror -P 2 --use-pget-n=1 --no-umask --no-perms "$VAR_SOURCE" "$VAR_DESTINATION"
+exit
+EOF
+else
+  lftp <<EOF
+set ftp:list-options -a
+set sftp:auto-confirm yes
+set cmd:fail-exit yes
+get "$VAR_SOURCE" -o "$VAR_DESTINATION"
+exit
+EOF
+fi
+if [ $? -eq 0 ]; then
+    echo "Seems like the filetransfer finished successfully (Exit code 0)"
+fi
+}
+
+if [[ -n "$VAR_USERNAME" && -n "$VAR_PASSWORD" ]]; then
+  echo "Authentication activated, username and password will be used"
+  lftp_authentication
+else
+  echo "Authentication deactivated, no username or password will be used"
+  lftp_no_authentication
+fi
